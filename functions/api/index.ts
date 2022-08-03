ccb18647d0d29546aeb272c7f97f5439b924c38b
Fabricio Silva <hi@fabricio.dev>
2022-08-03 09:21:34 +0100

    Add some start code to create endpoint

--- functions/api/index.ts
+++ functions/api/index.ts
@@ -1,27 +1,13 @@
-export async function onRequestPost(context) {
-  // Contents of context object
-  const {
-    request, // same as existing Worker API
-    env, // same as existing Worker API
-    params, // if filename includes [id] or [[path]]
-    waitUntil, // same as ctx.waitUntil in existing Worker API
-    next, // used for middleware or to fetch assets
-    data, // arbitrary space for passing data between middlewares
-  } = context
+// POST /api
+export const onRequestPost: PagesFunction = async context => {
+  const formData = await context.request.formData()
+
+  console.log(formData)
 
   return new Response("hit post api")
 }
 
-export async function onRequest(context) {
-  // Contents of context object
-  const {
-    request, // same as existing Worker API
-    env, // same as existing Worker API
-    params, // if filename includes [id] or [[path]]
-    waitUntil, // same as ctx.waitUntil in existing Worker API
-    next, // used for middleware or to fetch assets
-    data, // arbitrary space for passing data between middlewares
-  } = context
-
+// GET /api
+export const onRequest: PagesFunction = async context => {
   return new Response("hit get api")
 }
---
+++ public/main.js
@@ -0,0 +1,68 @@
+async function generateKey() {
+  return await crypto.subtle.generateKey(
+    {
+      name: "AES-GCM",
+      length: 256, // 128, 192, or 256
+    },
+    true,
+    ["encrypt", "decrypt"]
+  )
+}
+
+function generateIv() {
+  // Don't re-use initialization vectors
+  // Always generate a new iv every time your encrypt
+  // Recommended to use 12 bytes length
+  // Needed to decrypt
+  return crypto.getRandomValues(new Uint8Array(12))
+}
+
+async function exportKey(key) {
+  return await crypto.subtle.exportKey("jwk", key)
+}
+
+async function importKey(jwk) {
+  return await crypto.subtle.importKey("jwk", jwk)
+}
+
+async function encrypt(string, key, iv) {
+  return await crypto.subtle.encrypt(
+    { name: "AES-GCM", iv },
+    key,
+    new TextEncoder().encode(string)
+  )
+}
+
+async function decrypt(encrypted, iv, key) {
+  let decrypted = await crypto.subtle.decrypt(
+    { name: "AES-GCM", iv },
+    key,
+    encrypted
+  )
+  return new TextDecoder().decode(decrypted)
+}
+
+window.addEventListener("DOMContentLoaded", async () => {
+  const $inputKey = document.querySelector("input#key")
+  const $cryptButton = document.querySelector("button#go")
+  const $contentInput = document.querySelector("textarea#content")
+
+  const cryptoKey = await generateKey()
+  const jsonKey = await exportKey(cryptoKey)
+
+  $inputKey.value = jsonKey.k
+
+  $cryptButton.addEventListener("click", async () => {
+    const iv = generateIv()
+    const message = await encrypt($contentInput.value, cryptoKey, iv)
+
+    const formData = new FormData()
+    formData.append("iv", iv)
+    formData.append("message", new Blob([message]))
+
+    fetch("/api", {
+      method: "POST",
+      body: formData,
+    })
+  })
+})
